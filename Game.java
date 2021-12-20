e17da57575bb59673fd9335cb30a58816894a1bd
C110181123 <93572285+Stanley0919@users.noreply.github.com>
2021-12-21 00:31:01 +0800

    Add files via upload
    富淇commit

---
+++ Game.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Game {
+    public static int chooseCareer;
+
+    public static void main(String[] args) {
+        berserker Berserker =new berserker("狂戰士",30,5,15,"斬擊","橫掃");
+        magician Magician =new magician("魔法師",10,40,30,"治癒","小火球");
+        Archer Archer = new Archer("射手" ,10 ,30,40,"連射","隱形");
+        System.out.println("輸入職業代碼：1.狂戰士 2.弓箭手 3.魔法師");
+        Scanner chooseCareer = new Scanner(System.in);
+        if(chooseCareer.hasNextInt()){
+            switch (chooseCareer.nextInt()){
+                case 1:
+                    Berserker.getCurrentStatus();
+                case 2:
+                    Archer.getCurrentStatus();
+                case 3:
+                    Magician.getCurrentStatus();
+            }
+        }
+        else{
+            System.out.println("代碼輸入錯誤");
+        }
+    }
+}
